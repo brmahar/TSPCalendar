6de8acd5269ec8b75a716697c821bb539a8c29c6
brmahar <brmahar@mtu.edu>
2013-09-26 21:18:51 -0400

    Some not working sql stuff

--- src/CalendarMain.java
+++ src/CalendarMain.java
@@ -1,6 +1,9 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.swing.*;
 
@@ -45,6 +48,35 @@ public class CalendarMain extends JFrame {
 	public static void main(String[] args) {
 		CalendarMain theCal = new CalendarMain();
 		theCal.setVisible(true);
+		
+		System.out.println("-------- MySQL JDBC Connection Testing ------------");
+		 
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Where is your MySQL JDBC Driver?");
+			e.printStackTrace();
+			return;
+		}
+	 
+		System.out.println("MySQL JDBC Driver Registered!");
+		Connection connection = null;
+	 
+		try {
+			connection = DriverManager
+			.getConnection("jdbc:mysql://orion.csl.mtu.edu/brmahar","brmahar", "JMDRMzrjfxsFCxpJ");
+	 
+		} catch (SQLException e) {
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+		}
+	 
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
 	}
 
 }
