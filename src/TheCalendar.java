0bfeb0a367100f03c3da11f80f587668a2bb8f5b
ajbrowne <Dazztrazak@rover-209-125.rovernet.mtu.edu>
2013-10-02 18:16:25 -0400

    Day numbers in top left corner

--- src/TheCalendar.java
+++ src/TheCalendar.java
@@ -135,6 +135,11 @@ public class TheCalendar {
 			int column = (i+som-2)%7;
 			calendarTable.setValueAt(i, row, column);
 		}
+		DefaultTableCellRenderer render = new DefaultTableCellRenderer();
+		render.setVerticalAlignment(JLabel.TOP);
+		for (int i = 0; i < 7; i++){
+			Calendar.getColumnModel().getColumn(i).setCellRenderer(render);
+		}
 	}
 
 	static class btnPrev_Action implements ActionListener{
