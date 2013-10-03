99f0e5d9413b4e295926b74208498ebcf51c0176
ajbrowne <Dazztrazak@rover-209-125.rovernet.mtu.edu>
2013-10-02 21:39:50 -0400

    WeekView things

--- src/WeekView.java
+++ src/WeekView.java
@@ -6,15 +6,13 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
 
-public class TheCalendar {
+public class WeekView {
 
 	static JLabel month;
-	static JLabel year;
 	static JButton prev;
 	static JButton next;
 	static JTable Calendar;
 	@SuppressWarnings("rawtypes")
-	static JComboBox yearBox;
 	static JFrame mainFrame;
 	static Container thePane;
 	static DefaultTableModel calendarTable;
@@ -25,9 +23,10 @@ public class TheCalendar {
 	static int theYear;
 	static int otherMonth;
 	static int otherYear;
+	static int otherDay;
 
 	@SuppressWarnings("unchecked")
-	TheCalendar(){
+	WeekView(){
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		}
@@ -44,10 +43,8 @@ public class TheCalendar {
 
 		}
 
-		mainFrame = new JFrame("Month View");
-		month = new JLabel("January");
-		year = new JLabel("Change Year:");
-		yearBox = new JComboBox();
+		mainFrame = new JFrame("Week View");
+		month = new JLabel("Week of September 29th");
 		prev = new JButton("<-");
 		next = new JButton("->");
 		calendarTable = new DefaultTableModel()
@@ -70,16 +67,12 @@ public class TheCalendar {
 
 		thePane.add(calendarPanel);
 		calendarPanel.add(month);
-		calendarPanel.add(year);
-		calendarPanel.add(yearBox);
 		calendarPanel.add(prev);
 		calendarPanel.add(next);
 		calendarPanel.add(calendarScroll);
 
 		calendarPanel.setBounds(0, 0, 640, 670);
 		month.setBounds(320-month.getPreferredSize().width/2, 50, 200, 50);
-		year.setBounds(20, 610, 160, 40);
-		yearBox.setBounds(460, 610, 160, 40);
 		prev.setBounds(20, 50, 100, 50);
 		next.setBounds(520, 50, 100, 50);
 		calendarScroll.setBounds(20, 100, 600, 500);
@@ -91,6 +84,7 @@ public class TheCalendar {
 		theDay = gregCal.get(GregorianCalendar.DAY_OF_MONTH);
 		theMonth = gregCal.get(GregorianCalendar.MONTH);
 		theYear = gregCal.get(GregorianCalendar.YEAR);
+		otherDay = theDay;
 		otherMonth = theMonth;
 		otherYear = theYear;
 
@@ -105,56 +99,32 @@ public class TheCalendar {
 		Calendar.setColumnSelectionAllowed(true);
 		Calendar.setRowSelectionAllowed(true);
 		Calendar.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		Calendar.setRowHeight(76);
+		Calendar.setRowHeight(456);
 		calendarTable.setColumnCount(7);
-		calendarTable.setRowCount(6);
-
-		for (int i = theYear-100; i < theYear+100; i++){
-			yearBox.addItem(String.valueOf(i));
-		}
+		calendarTable.setRowCount(1);
 
 		prev.addActionListener(new prevMonth());
 		next.addActionListener(new nextMonth());
-		yearBox.addActionListener(new changeYear());
 
 		updateCalendar(theMonth, theYear);
 	}
 
-	public static void updateCalendar(int aMonth, int aYear){
-		String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-		int days;
-		int startOfMonth; 
+	public static void updateCalendar(int aDay, int aMonth){
+		String[] months = {"Sudday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+		int startOfWeek; 
 
 		prev.setEnabled(true); 
 		next.setEnabled(true);
 
-		if (aMonth == 0 && aYear <= theYear-10){
-			prev.setEnabled(false);
-		} 
-		if (aMonth == 11 && aYear >= theYear+100){
-			next.setEnabled(false);
-		} 
 		month.setText(months[aMonth]); 
 		month.setBounds(320-month.getPreferredSize().width/2, 50, 360, 50); 
-		yearBox.setSelectedItem(String.valueOf(aYear)); 
 
-		GregorianCalendar gregCal = new GregorianCalendar(aYear, aMonth, 1);
-		days = gregCal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
-		startOfMonth = gregCal.get(GregorianCalendar.DAY_OF_WEEK);
+		//GregorianCalendar gregCal = new GregorianCalendar(aYear, aMonth, 1);
+		//startOfWeek = gregCal.get(GregorianCalendar.DAY_OF_WEEK);
 
-		for (int i = 0; i < 6; i++){
-			for (int j = 0; j < 7; j++){
-				calendarTable.setValueAt(null, i, j);
-			}
-		}
-
-		for (int i = 1; i <= days; i++){
-			int row = new Integer((i+startOfMonth-2)/7);
-			int column = (i+startOfMonth-2)%7;
-			String template = "<html>%s<br>Event<br>Event<br>Event<html>";
-			String stringA = String.valueOf(i);
-			String put = String.format(template, stringA);
-			calendarTable.setValueAt(put, row, column);
+		for (int j = 0; j < 7; j++){
+			String template = "<html>Event<br>Event<br>Event<html>";
+			calendarTable.setValueAt(template, 0, j);
 		}
 
 		DefaultTableCellRenderer render = new DefaultTableCellRenderer();
@@ -187,15 +157,6 @@ public class TheCalendar {
 				otherMonth += 1;
 			}
 			updateCalendar(otherMonth, otherYear);
-		}
-	}
-	static class changeYear implements ActionListener{
-		public void actionPerformed (ActionEvent e){
-			if (yearBox.getSelectedItem() != null){
-				String b = yearBox.getSelectedItem().toString();
-				otherYear = Integer.parseInt(b);
-				updateCalendar(otherMonth, otherYear);
-			}
 		}
 	}
 
