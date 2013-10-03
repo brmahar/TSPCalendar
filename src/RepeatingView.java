a89afb10ea237cee6ac8a800e08a35ee4baaefc4
ajbrowne <Dazztrazak@rover-211-182.rovernet.mtu.edu>
2013-10-03 19:22:05 -0400

    errors

--- src/RepeatingView.java
+++ src/RepeatingView.java
@@ -3,6 +3,7 @@ import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Arrays;
 
 import javax.swing.*;
 
@@ -58,7 +59,9 @@ public class RepeatingView extends JFrame {
 				if (saturday.isSelected()){
 					dayArray[6] = 1;
 				}
+				//System.out.println(Arrays.toString(dayArray));
 				AddEvent theView = new AddEvent(data, thisThing, dayArray);
+				theView.setVisible(true);
 			}
 		});
 		
