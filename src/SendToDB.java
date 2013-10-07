525dab940d1f7419aba137f6511ac433800ba1a8
brmahar <brmahar@mtu.edu>
2013-10-06 21:59:52 -0400

    Comments all around

--- src/SendToDB.java
+++ src/SendToDB.java
@@ -1,49 +1,47 @@
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
 import java.util.UUID;
 
-import javax.swing.*;
-
 import com.mysql.jdbc.PreparedStatement;
 
-
+/**
+ * This class is used to connect to the database and to send or retrieve data as needed.
+ * 
+ * 
+ * @author Brady Mahar
+ *
+ */
 public class SendToDB {
 
-
+	//This method is used to setup the actual connection with the database
 	public void runStore(StoreData data, int bool){
 
-
-		System.out.println("------------ MySQL JDBC Connection Testing ------------");
-
+		//This try catch block is used to setup the driver used to connect to the database
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e) {
-			System.out.println("No MySQL JDBC Driver?");
 			e.printStackTrace();
 			return;
 		}
 
-		System.out.println("MySQL JDBC Driver Registered");
 		Connection connection = null;
-
+		
+		//This try catch block is used to connect to the database using the driver
 		try {
 			connection = DriverManager
 					.getConnection("jdbc:mysql://orion.csl.mtu.edu/ajbrowne","ajbrowne", "ajZ4VikY/tnI.");
 
 		} catch (SQLException e) {
-			System.out.println("Connection Failed!");
 			((Throwable) e).printStackTrace();
 			return;
 		}
-
+		
+		//This block first checks to see which of the following methods should be used.
 		if (connection != null) {
-			System.out.println("Now Connected, so please stay and look around!");
 			if(bool == 1){
 				getData(connection,data);
 			}else if(bool == 2){
@@ -68,15 +66,25 @@ public class SendToDB {
 		} else {
 			System.out.println("Failed to make a connection!");
 		}
+		//This try catch block just closes the connection to the database 
 		try {
 			connection.close();
 		} catch (SQLException e) {
-			System.out.println("The connection was not closed.....Run away now!!!!");
 			e.printStackTrace();
 		}
 	}
-
+	
+	
+	/**
+	 * This method is used to store data in the data base
+	 * It stores the data that is given to it by the addEvent class
+	 * 
+	 * @param connection
+	 * @param data
+	 */
 	public void send(Connection connection, StoreData data){
+		//The following code prepares a statement to be sent to the database
+		//and gets all of the data from a Storedata object
 		PreparedStatement preStmt=null;
 		StoreData theData = data;
 		String name = theData.getName();
@@ -88,7 +96,7 @@ public class SendToDB {
 		String theETime = theData.getETime();
 		String theID = UUID.randomUUID().toString();
 
-
+		//This try catch block attempts to send a sql statement to the database to store the given data
 		try {
 			preStmt = (PreparedStatement) connection.prepareStatement("INSERT INTO "
 					+ "Event(Name,Location,Description,End_Date, Start_Date, Start_Time, End_Time, id) VALUES(?,?,?,?,?,?,?,?)"); 
@@ -106,20 +114,32 @@ public class SendToDB {
 			preStmt.setString(7, theETime);
 			preStmt.executeUpdate();
 		} catch (SQLException | ParseException e) {
-			System.out.println("Nothing was added lawllawllawl");
+			System.out.println("Nothing was added.");
 			e.printStackTrace();
 		}
 
 	}
-
+	
+	/**
+	 * This method is used to get data from the database based on the name given
+	 * 
+	 * @param connection
+	 * @param data
+	 */
 	public void getData(Connection connection, StoreData data){
+		//This first set of code prepares a statement to get the data from the database
 		PreparedStatement preStmt=null;
 		StoreData theData = data;
+		//This code prepares some date formats to convert what is typed in
 		SimpleDateFormat displayDate = new SimpleDateFormat("MM-dd-yyyy");
 		SimpleDateFormat dbDate = new SimpleDateFormat("yyyy-MM-dd");
 		String formatSDate = null;
 		String formatEDate = null;
+		//The following is the actual sql statement to be used
 		String stmt = "SELECT * FROM Event WHERE Name= ?";
+		
+		//The following try catch block gets all of the events that have the same name as the name typed in
+		//It loops through a result set that is returned from the database storing the data in store data objects
 		try {
 			preStmt = (PreparedStatement) connection.prepareStatement(stmt);
 			preStmt.setString(1, data.getName());
@@ -129,6 +149,7 @@ public class SendToDB {
 				String location = rs.getString("Location");
 				String date = rs.getString("Start_Date");
 				String endDate = rs.getString("End_Date");
+				//This try catch block deals with formatting the date correctly 
 				try {
 
 					formatSDate = displayDate.format(dbDate.parse(date));
@@ -146,35 +167,54 @@ public class SendToDB {
 				data.setETime(eTime);
 			}
 		} catch (SQLException e) {
-			System.out.println("Man you got problems now");
 			e.printStackTrace();
 		}
 
 	}
 
+	/**
+	 * This method is used to delete an event from the database based on name
+	 * 
+	 * @param connection
+	 * @param data
+	 */
 	public void deleteEvent(Connection connection, StoreData data){
+		//First a statement is prepared to send to the database to delete the events 
 		PreparedStatement preStmt=null;
 		String stmt = "DELETE FROM Event WHERE id=?";
+		//This try catch block is used to send the delete command to the database
 		try{
 			preStmt = (PreparedStatement) connection.prepareStatement(stmt);
 			preStmt.setString(1, data.getID());
 			preStmt.executeUpdate();
 		}catch(SQLException e){
-			System.out.println("Man you got problems now");
 			e.printStackTrace();
 		}
 	}
-
+	
+	/**
+	 * This method is used to get an event from the database based on the 
+	 * start date of the event
+	 * 
+	 * @param connection
+	 * @param data
+	 * @param bool
+	 */
 	public void getSpecificData(Connection connection, StoreData data, int bool){
+		//This code is used to prepare a statement to send to the database based on the start date
 		PreparedStatement preStmt=null;
 		String stmt = "SELECT * FROM Event WHERE Start_Date = (?)";
 		String formatSDate = null;
 		String formatEDate = null;
 		StoreData extra = new StoreData();
+		
+		//This try catch block is used to format the date to be sent to the database
+		//The command is then sent to get the correct events
 		try{
 			preStmt = (PreparedStatement) connection.prepareStatement(stmt);
 			SimpleDateFormat displayDate = new SimpleDateFormat("MM-dd-yyyy");
 			SimpleDateFormat dbDate = new SimpleDateFormat("yyyy-MM-dd");
+			//formats the date to match the database
 			try {
 
 				formatSDate = dbDate.format(displayDate.parse(data.getDate()));
@@ -183,9 +223,11 @@ public class SendToDB {
 			}
 			preStmt.setString(1, formatSDate);
 			ResultSet rs = preStmt.executeQuery();
+			//re-adds the correct events back to the storedata object to be used later
 			for(int i = 0; i < data.getMultiDay().size();i++){
 				data.addName(data.getMultiDay().get(i).getName());
 			}
+			//loops through the result set returned by the database call
 			while(rs.next()){
 				String description = rs.getString("Description");
 				String location = rs.getString("Location");
@@ -193,7 +235,7 @@ public class SendToDB {
 				String endDate = rs.getString("End_Date");
 				String sTime = rs.getString("Start_Time");
 				String eTime = rs.getString("End_Time");
-
+				//This try catch is used to format the date correctly
 				try {
 					formatEDate = displayDate.format(dbDate.parse(endDate));
 				} catch (ParseException e) {
@@ -213,22 +255,32 @@ public class SendToDB {
 				extra.setLocation(location);
 				extra.setSTime(sTime);
 				extra.setETime(eTime);
+				//adds the event to the multi-day arraylist to be used later
 				if(!data.getDate().equals(data.getEndDate())){
 					data.addEvent(extra);
 				}
 
 			}
 		}catch(SQLException e){
-			System.out.println("Man you got problems now");
 			e.printStackTrace();
 		}
 
 	}
-
+	
+	/**
+	 * This method is used to get events from the database based on the date
+	 * The sql command does not directly use the date
+	 * 
+	 * 
+	 * @param connection
+	 * @param data
+	 */
 	public void getDateEvents(Connection connection, StoreData data){
+		//The following code is used to prepare a sql statement to get all the events
 		data.resetSingle();
 		PreparedStatement preStmt=null;
 		String stmt = "SELECT * FROM Event";
+		//prepares the date given to this method to be used to get the correct events 
 		String passedDate = data.getDate();
 		SimpleDateFormat displayDate = new SimpleDateFormat("MM-dd-yyyy");
 		SimpleDateFormat dbDate = new SimpleDateFormat("yyyy-MM-dd");
@@ -249,11 +301,14 @@ public class SendToDB {
 		int startM = 0;
 		int curM = 0;
 		int endM = 0;
-
+		
+		//This try catch block is used to get the events that are returned based on
+		//the dates that the event has
 		try{
 			preStmt = (PreparedStatement) connection.prepareStatement(stmt);
 			ResultSet rs = preStmt.executeQuery();
 			StoreData newDay;
+			//loops through all of the events storing the ones that have the matching dates
 			while(rs.next()){
 				newDay = new StoreData();
 				String name = rs.getString("Name");
@@ -261,6 +316,7 @@ public class SendToDB {
 				String location = rs.getString("Location");
 				String date = rs.getString("Start_Date");
 				String endDate = rs.getString("End_Date");
+				//This try catch formats the dates to be used to check to see if they are correct
 				try {
 
 					formatSDate = displayDate.format(dbDate.parse(date));
@@ -289,7 +345,7 @@ public class SendToDB {
 				newDay.setEndDate(endDate);
 				newDay.setSTime(sTime);
 				newDay.setETime(eTime);
-
+				//stores the events that match the date that was given
 				if(curD >= startD && curD <= endD && curM == startM){
 					data.addDayEvent(newDay);
 				}else if(startM < curM && curM == endM && curD <= endD){
@@ -304,20 +360,31 @@ public class SendToDB {
 		}
 	}
 	
+	/**
+	 * This method is used to get events with a similar name
+	 * It is seperate from the one above because it gets all of the events with the same name
+	 * 
+	 * @param connection
+	 * @param data
+	 */
 	public void getNameEvents(Connection connection, StoreData data){
+		//This code prepares a statement to be sent to the database
 		data.resetSingle();
 		PreparedStatement preStmt=null;
 		String stmt = "SELECT * FROM Event";
+		//used to format the date from the database 
 		SimpleDateFormat displayDate = new SimpleDateFormat("MM-dd-yyyy");
 		SimpleDateFormat dbDate = new SimpleDateFormat("yyyy-MM-dd");
 		String formatSDate = null;
 		String formatEDate = null;
 		String passedName = data.getName();
-		
+		//This try catch is used to get the result set from the database which contains all of the events
+		//with the same name
 		try{
 			preStmt = (PreparedStatement) connection.prepareStatement(stmt);
 			ResultSet rs = preStmt.executeQuery();
 			StoreData newDay;
+			//Loops through the result set to get and store all of the events
 			while(rs.next()){
 				newDay = new StoreData();
 				String name = rs.getString("Name");
@@ -328,6 +395,7 @@ public class SendToDB {
 				String sTime = rs.getString("Start_Time");
 				String eTime = rs.getString("End_Time");
 				String id = rs.getString("id");
+				//formats the dates properly
 				try {
 
 					formatSDate = displayDate.format(dbDate.parse(date));
@@ -343,6 +411,7 @@ public class SendToDB {
 				newDay.setSTime(sTime);
 				newDay.setETime(eTime);
 				newDay.setID(id);
+				//stores an event based on name
 				if(name.equals(passedName)){
 					data.addDayEvent(newDay);
 				}
@@ -353,7 +422,15 @@ public class SendToDB {
 		}
 	}
 	
+	/**
+	 * This method is used to get events by the ID that is given to
+	 * them when they are first stored in the database
+	 * 
+	 * @param connection
+	 * @param data
+	 */
 	public void getByID(Connection connection, StoreData data){
+		//prepares a statement to send to the database and prepares date formatting 
 		PreparedStatement preStmt=null;
 		StoreData theData = data;
 		SimpleDateFormat displayDate = new SimpleDateFormat("MM-dd-yyyy");
@@ -361,15 +438,19 @@ public class SendToDB {
 		String formatSDate = null;
 		String formatEDate = null;
 		String stmt = "SELECT * FROM Event WHERE id= ?";
+		//This try catch block is used to make the sql call and to format the dates 
+		// and analyze the events to store them 
 		try {
 			preStmt = (PreparedStatement) connection.prepareStatement(stmt);
 			preStmt.setString(1, data.getID());
 			ResultSet rs = preStmt.executeQuery();
+			//loops through the result set to get the correct events
 			while(rs.next()){
 				String description = rs.getString("Description");
 				String location = rs.getString("Location");
 				String date = rs.getString("Start_Date");
 				String endDate = rs.getString("End_Date");
+				//This try catch block formats the date correctly
 				try {
 
 					formatSDate = displayDate.format(dbDate.parse(date));
@@ -392,7 +473,14 @@ public class SendToDB {
 		}
 	}
 	
+	/**
+	 * This method is used to edit an event based on the given name
+	 * 
+	 * @param connection
+	 * @param data
+	 */
 	public void editEvent(Connection connection, StoreData data){
+		//Prepares a statement to edit a full event based on the correct name
 		PreparedStatement preStmt=null;
 		StoreData theData = data;
 		String name = theData.getName();
@@ -403,7 +491,7 @@ public class SendToDB {
 		String theSTime = theData.getSTime();
 		String theETime = theData.getETime();
 		String id = theData.getID();
-		
+		//This try catch block is used to make the sql call and to edit the correct event
 		try {
 			preStmt = (PreparedStatement) connection.prepareStatement("UPDATE "
 					+ "Event SET Name=?, Location=?, Description=?, End_Date=?, Start_Date=?, Start_Time=?, End_Time=? WHERE id=?"); 
@@ -422,12 +510,20 @@ public class SendToDB {
 			preStmt.setString(8, id);
 			preStmt.executeUpdate();
 		} catch (SQLException | ParseException e) {
-			System.out.println("Nothing was added lawllawllawl");
+			System.out.println("Nothing was added.");
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+	 * This method is used to add repeat events to the database
+	 * 
+	 * @param connection
+	 * @param data
+	 */
 	public void addRepeats(Connection connection, StoreData data){
+		//This code is used to prepare a statement to be sent to the database
+		//getting the rest of the data from the current event
 		PreparedStatement preStmt=null;
 		StoreData theData = data;
 		String name = theData.getName();
@@ -438,8 +534,9 @@ public class SendToDB {
 		String theSTime = theData.getSTime();
 		String theETime = theData.getETime();
 		String theID = UUID.randomUUID().toString();
-
+		//This loop loops through the days adding the correct event to the correct days
 		for(int i = 0; i < data.getSingleDay().size(); i++){
+			//This try catch block is used to send a sql statemnet to the database updating the date correctly
 			try {
 				preStmt = (PreparedStatement) connection.prepareStatement("INSERT INTO "
 						+ "Event(Name,Location,Description,End_Date, Start_Date, Start_Time, End_Time, id) VALUES(?,?,?,?,?,?,?,?)"); 
@@ -457,7 +554,7 @@ public class SendToDB {
 				preStmt.setString(7, theETime);
 				preStmt.executeUpdate();
 			} catch (SQLException | ParseException e) {
-				System.out.println("Nothing was added lawllawllawl");
+				System.out.println("Nothing was added");
 				e.printStackTrace();
 			}
 		}
--- src/StoreData.java
+++ src/StoreData.java
@@ -1,8 +1,14 @@
 import java.util.ArrayList;
 
-
+/**
+ * This class is used as a data object to store specific data about the event
+ * 
+ * @author Brady Mahar
+ *
+ */
 public class StoreData {
-
+	
+	//These are the variables used to store the correct data
 	private String name = null;
 	private String location = null;
 	private String description = null;
@@ -17,94 +23,115 @@ public class StoreData {
 	private ArrayList<StoreData> singleDay = new ArrayList<StoreData>();
 	
 
-
+	//This is the setter for the ID
 	public void setID(String theID){
 		id = theID;
 	}
+	//This is the setter for the name
 	public void setName(String theName){
 		name = theName;
 	}
+	//This is the setter for the Location
 	public void setLocation(String theLocal){
 		location = theLocal;
 	}
+	//This is the setter for the description
 	public void setDescription(String theDescription){
 		description= theDescription;
 	}
+	//This is the setter for the start date
 	public void setDate(String theDate){
 		date = theDate;
 	}
+	//This is the setter for the end date
 	public void setEndDate(String theDate){
 		endDate = theDate;
 	}
+	//This is the setter for the start time
 	public void setSTime(String theTime){
 		sTime = theTime;
 	}
+	//This is the setter for the end time
 	public void setETime(String theTime){
 		eTime = theTime;
 	}
+	//This is the setter for the array list of dates for repeat events 
 	public void addDates(String newDate){
 		sDates.add(newDate);
 	}
-	
+	//This is the setter for the array list of names for multiple names
 	public void addName(String newName){
 		names.add(newName);
 	}
-	
+	//This is the setter for the array list to store multi day events
 	public void addEvent(StoreData newEvent){
 		multiDay.add(newEvent);
 	}
+	//This is the setter for the array list for single day events
 	public void addDayEvent(StoreData newEvent){
 		singleDay.add(newEvent);
 	}
+	//Resets the names array list
 	public void resetNames(){
 		names.clear();
 	}
+	//This is the getter for the name
 	public String getName(){
 		return name;
 	}
+	//This is the getter for the location
 	public String getLocation(){
 		return location;
 	}
+	//This is the getter for the description
 	public String getDescription(){
 		return description;
 	}
+	//This is the getter for the start date
 	public String getDate(){
 		return date;
 	}
+	//This is the getter for the end date
 	public String getEndDate(){
 		return endDate;
 	}
+	//This is the getter for the start time
 	public String getSTime(){
 		return sTime;
 	}
+	//This is the getter for the end time
 	public String getETime(){
 		return eTime;
 	}
-	
+	//This is the getter for the names array list
 	public ArrayList<String> getNames(){
 		return names;
 	}
+	//This is the getter for the dates array list
 	public ArrayList<String> getDates(){
 		return sDates;
 	}
-	
+	//This is the getter for the multi day array list
 	public ArrayList<StoreData>getMultiDay(){
 		return multiDay;
 	}
+	//removes data from the multi day array list
 	public void removeData(StoreData remove){
 		multiDay.remove(remove);
 	}
-	
+	//This is the getter for the single day 
 	public ArrayList<StoreData>getSingleDay(){
 		return singleDay;
 	}
+	//Removes single day events from the arraylist
 	public void removeDayEvent(StoreData remove){
 		singleDay.remove(remove);
 	}
+	//resets the single day array list
 	public void resetSingle(){
 		singleDay.clear();
 	}
-	
+	//This is the getter for the ID
 	public String getID(){
 		return id;
 	}
